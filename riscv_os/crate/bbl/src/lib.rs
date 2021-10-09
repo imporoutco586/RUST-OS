891b3595fbaef3700586322b9a57b4860c600fb5
imporoutco586 <85064183+imporoutco586@users.noreply.github.com>
2021-10-10 02:55:26 +0800

    Add files via upload

---
+++ riscv_os/crate/bbl/src/lib.rs
@@ -0,0 +1,5 @@
+#![no_std]
+#![feature(asm)]
+#![feature(llvm_asm)]
+#[warn(dead_code)]
+pub mod sbi;
---
+++ riscv_os/src/main.rs
@@ -0,0 +1,29 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+#![feature(global_asm)]
+
+#[macro_use]
+pub mod io;
+
+use core::panic::PanicInfo;
+
+global_asm!(include_str!("boot/entry.asm"));
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn rust_main() -> ! {
+    let a = "Hello";
+    let b = "World";
+    println!("{}, {}!", a, b);
+    panic!("End of rust_main");
+}
+
+#[no_mangle]
+pub extern fn abort() {
+    panic!("abort!");
+}
